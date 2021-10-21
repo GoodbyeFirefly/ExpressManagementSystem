8a139927593ce0185a2bfb59895acd50ac1859c4
GoodbyeFirefly <1961192090@qq.com>
2021-10-21 22:17:27 +0800

    完成快递员历史查询功能：根据快递员的号码，在express表中查询相关快件并展示（效果类似于快件列表）； 发现一个问题：快件取出后，列表中快件录入的信息就不再展示了，快件列表功能也有这种问题，暂时还未修复；

--- src/com/xxy/wx/controller/ExpressController.java
+++ src/com/xxy/wx/controller/ExpressController.java
@@ -157,5 +157,58 @@ public class ExpressController {
         return JSONUtil.toJSON(msg);
     }
 
+    @ResponseBody("/wx/findExpressBySysPhone.do")
+    public String findBySysPhone(HttpServletRequest request, HttpServletResponse response) {
+        User wxUser = UserUtil.getWxUser(request.getSession());
+        String userphone = wxUser.getUserphone();
+        List<Express> expresses = ExpressService.findBySysPhone(userphone);
+        List<BootstrapTableExpress> list2 = new ArrayList<>();
+        for(Express e:expresses){
+            String inTime = DateFormatUtil.format(e.getIntime());
+            String outTime = e.getOuttime()==null?"未出库":DateFormatUtil.format(e.getOuttime());
+            String status = e.getStatus()==0?"待取件":"已取件";
+            String code = e.getCode()==null?"已取件":e.getCode();
+            BootstrapTableExpress e2 = new BootstrapTableExpress(e.getId(),e.getNumber(),e.getUsername(),e.getUserphone(),e.getCompany(),code,inTime,outTime,status,e.getSysPhone());
+            list2.add(e2);
+        }
 
+        Message msg = new Message();
+        if (expresses.size() == 0) {
+            msg.setStatus(-1);
+        } else {
+            msg.setStatus(0);
+            // 从对象列表中挑选出status为0的快递对象，并按入库时间进行排序
+            Stream<BootstrapTableExpress> status0Express = list2.stream().filter(express -> {
+                if (express.getStatus().equals("待取件")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }).sorted((o1,o2) -> {
+                long o1Time = DateFormatUtil.toTime(o1.getIntime());
+                long o2Time = DateFormatUtil.toTime(o2.getIntime());
+                return (int)(o1Time-o2Time);
+            });
+            Stream<BootstrapTableExpress> status1Express = list2.stream().filter(express -> {
+                if (express.getStatus().equals("已取件")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }).sorted((o1,o2) -> {
+                long o1Time = DateFormatUtil.toTime(o1.getIntime());
+                long o2Time = DateFormatUtil.toTime(o2.getIntime());
+                return (int)(o1Time-o2Time);
+            });
+            Object[] s0 = status0Express.toArray();
+            Object[] s1 = status1Express.toArray();
+            Map data = new HashMap<>();
+            data.put("status0",s0);
+            data.put("status1",s1);
+            msg.setData(data);
+        }
+        return JSONUtil.toJSON(msg);
+    }
 }
+
+
